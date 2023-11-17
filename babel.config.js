abdb59a9b0f59a66f5c1d983454c86784600a071
tiendm <tiendm@runsystem.net>
2023-11-17 16:02:17 +0700

    4rd commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    presets: ['@babel/preset-env'],
+    presets: ['@babel/preset-env',
+    {
+      targets: {
+        node: 'current'
+      }
+    }
+  ],
     "transformIgnorePatterns": [
         "[/\\\\]node_modules[/\\\\].+[^esm]\\.(js|jsx|mjs|cjs|ts|tsx)$",
         "^.+\\.module\\.(css|sass|scss)$"
